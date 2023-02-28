27616a702a8ad9904026a40150bf57a3c2fd2310
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-28 13:39:50 +0400

    Add disconnect event in client.py

--- client.py
+++ client.py
@@ -4,32 +4,34 @@ import threading
 DISCONNECT_MESSAGE = "!bb"
 
 
-def send_message(client: socket.socket) -> None:
+def send_message(client: socket.socket, disconnect: threading.Event) -> None:
     while True:
         try:
             message = input().strip()
+            if disconnect.is_set():
+                break
             if message == DISCONNECT_MESSAGE:
-                disconnect_client(client)
+                disconnect_client(client, disconnect)
                 break
             if message:
                 client.send(message.encode("utf-8"))
 
         except socket.error as e:
             print("[LOSE_CONNECTION_SEND]")
-            disconnect_client(client)
+            disconnect_client(client, disconnect)
             break
         except Exception as e:
             print("[ERROR_DURING_SEND] :", e)
             break
 
 
-def recieve_message(client: socket.socket) -> None:
+def recieve_message(client: socket.socket, disconnect: threading.Event) -> None:
     while True:
         try:
             data = client.recv(1024)
             if len(data) == 0:
                 print(f"\nINFO: Server was shutdown")
-                disconnect_client(client)
+                disconnect_client(client, disconnect)
                 break
             message = data.decode("utf-8")
 
@@ -38,23 +40,26 @@ def recieve_message(client: socket.socket) -> None:
 
         except socket.error as e:
             print("[LOSE_CONNECTION_RECV]")
-            disconnect_client(client)
+            disconnect_client(client, disconnect)
             break
         except Exception as e:
             print("[ERROR_DURING_RECV] :", e)
             break
 
 
-def disconnect_client(client: socket.socket) -> None:
+def disconnect_client(client: socket.socket, disconnect: threading.Event) -> None:
+    disconnect.set()
     client.close()
     print("\n-= See you later! =-")
 
 
 def start_send_recieve_threads(client: socket.socket) -> None:
-    t1 = threading.Thread(target=send_message, args=(client,))
+    disconnect = threading.Event()
+
+    t1 = threading.Thread(target=send_message, args=(client, disconnect))
     t1.start()
 
-    t2 = threading.Thread(target=recieve_message, args=(client,))
+    t2 = threading.Thread(target=recieve_message, args=(client, disconnect))
     t2.daemon = True
     t2.start()
 
